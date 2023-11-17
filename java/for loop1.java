36c3e5c67256196019887775f43e0c502448f2f3
africaelija <151010848+africaelija@users.noreply.github.com>
2023-11-17 12:01:22 +0200

    Add files via upload

---
+++ java/for loop1.java
@@ -0,0 +1,11 @@
+class for loop1.java
+{
+public static void main(string args[])
+{
+{
+for(int x=0; x++)
+{
+system.out,println(x);
+}
+}
+}
---
+++ java/forloop1.java
@@ -0,0 +1,10 @@
+class forloop
+{
+public static void main(String args[])
+{
+for(int x=0;x<7; x++)
+{
+System.out.println(x);
+}
+}
+}
